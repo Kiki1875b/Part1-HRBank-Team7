81277abc9979d04fce9a497fdd68f9a726435be0
Kiki1875b <kevinheo0413@gmail.com>
2025-03-20 21:26:33 +0900

    feat : added EmployeeStatisticRepository

---
+++ src/main/java/team7/hrbank/domain/emplyee_statistic/EmployeeStatisticRepository.java
@@ -0,0 +1,7 @@
+package team7.hrbank.domain.emplyee_statistic;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeStatisticRepository extends JpaRepository<EmployeeStatistic, Long> {
+
+}
